3f6c0cc10e41c9458a3ffd0b76793910fced9bc9
Tom C <mysophobicgamer@gmail.com>
2022-04-26 19:15:20 -0400

    work

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
+let header = document.querySelector('#page-header')
+header.style.textAlign = "left"
 
-// Select some elements...
+let dogDiscription = document.querySelectorAll('.dog-description')
+for (let i =0; i < dogDiscription.length; i++) {
+    dogDiscription[i].style.color = "red"
+}
+
+let dogName = document.querySelectorAll('.dog-name')
+for(let i = 0; i < dogName.length; i++){
+	dogName[i].style.textAlign = "left"
+}
+
+let dogImages = document.querySelectorAll('.dog-image')
+for(let i = 0; i < dogImages.length; i++){
+	dogImages[i].style.borderRadius = '50px'
+    dogImages[i].style.transform = 'rotate(180deg)'
+}
